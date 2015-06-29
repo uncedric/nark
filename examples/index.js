6cae53d2277ec4514a73c9b762a867cd9db971e8
Cedric <cedric@intrabits.net>
2015-06-29 12:57:24 -0500

    Print objects correctly + add examples

--- examples/index.js
+++ examples/index.js
@@ -4,6 +4,16 @@ nark = Nark.config({});
 
 nark.log('Hola bartola Ü');
 
+nark.log({foo:'12',bar:'13'});
+
+nark.log(['hey there']);
+
+var hey = function () {
+  var hola = 'asd';
+};
+
+nark.log(hey);
+
 nark.warning('Warning!!');
 
 nark.danger('There was an error somewhere',{foo:'hola',bar:'bartola'});
